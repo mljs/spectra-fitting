f7844f2704c87d88927f1967848f537c76b26a5e
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-10-31 02:28:31 -0500

    chore: update documentation

--- src/optimize.js
+++ src/optimize.js
@@ -2,12 +2,12 @@ import { optimizeSum } from './optimizeSum';
 
 /**
  * Fits a set of points to a bell function.
- * @param {Object} data - A object containing the x and y data to be fitted.
- * @param {Array} peak - A object of initial parameters to be optimized comming from a peak picking {x, y, width}.
+ * @param {Object} data - An object containing the x and y data to be fitted.
+ * @param {Object} peak - An object of initial parameters to be optimized. e.g. coming from a peak picking {x, y, width}.
  * @param {Object} [options = {}]
  * @param {String} [options.kind = 'gaussian'] - kind of shape used to fitting, lorentzian, gaussian and pseudovoigt are supported.
  * @param {Object} [options.lmOptions = {}] - options of ml-levenberg-marquardt optimization package.
- * @returns {Object} - A object with fitting error and the list of optimized parameters { parameters: [x, y, width], error } if the kind of shape is pseudoVoigt mu parameter is optimized.{Array} - A set of objects of optimized parameters { parameters: [x, y, width], error } if the kind of shape is pseudoVoigt mu parameter is optimized.
+ * @returns {Object} - An object with fitting error and the optimized parameters { parameters: {x, y, width}, error } if the kind of shape is pseudoVoigt mu parameter is optimized.{Array} - A set of objects of optimized parameters { parameters: [x, y, width], error } if the kind of shape is pseudoVoigt mu parameter is optimized.
  */
 export function optimize(data, peak, options = {}) {
   let result = optimizeSum(data, [peak], options);
--- src/optimizeSum.js
+++ src/optimizeSum.js
@@ -8,12 +8,12 @@ import { sumOfLorentzians } from './shapes/sumOfLorentzians';
 const keys = ['x', 'y', 'width', 'mu'];
 /**
  * Fits a set of points to the sum of a set of bell functions.
- * @param {Object} input - A object containing the x and y data to be fitted.
- * @param {Array} group - A list of initial parameters to be optimized comming from a peak picking {x, y, width}.
+ * @param {Object} input - An object containing the x and y data to be fitted.
+ * @param {Array} group - A list of initial parameters to be optimized. e.g. coming from a peak picking [{x, y, width}].
  * @param {Object} [options = {}]
  * @param {String} [options.kind = 'gaussian'] - kind of shape used to fitting, lorentzian, gaussian and pseudovoigt are supported.
  * @param {Object} [options.lmOptions = {}] - options of ml-levenberg-marquardt optimization package.
- * @returns {Object} - A object with fitting error and the list of optimized parameters { parameters: [x, y, width], error } if the kind of shape is pseudoVoigt mu parameter is optimized.
+ * @returns {Object} - A object with fitting error and the list of optimized parameters { parameters: [ {x, y, width} ], error } if the kind of shape is pseudoVoigt mu parameter is optimized.
  */
 export function optimizeSum(data, group, options = {}) {
   let { kind = 'gaussian', lmOptions = {} } = options;
