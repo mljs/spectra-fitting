61470823ad0e2c9cf7f95a6c730a388197aa075d
jobo322 <jose.bolanos@correounivalle.edu.co>
2020-10-31 03:17:17 -0500

    chore: use width as FWHM instead of standard deviation of gaussian shape

--- src/shapes/sumOfGaussianLorentzians.js
+++ src/shapes/sumOfGaussianLorentzians.js
@@ -7,6 +7,7 @@
  */
 
 export function sumOfGaussianLorentzians(p) {
+  const factor = 2 / Math.sqrt(2 * Math.log(2));
   return function (t) {
     let nL = p.length / 4;
     let result = 0;
@@ -15,10 +16,10 @@ export function sumOfGaussianLorentzians(p) {
       let xL = 1 - xG;
       let p2 = Math.pow(p[i + nL * 2] / 2, 2);
       let factorL = xL * p[i + nL] * p2;
-      let factorG1 = p[i + nL * 2] * p[i + nL * 2] * 2;
-      let factorG2 = xG * p[i + nL];
+      let factorG = xG * p[i + nL];
       result +=
-        factorG2 * Math.exp(-Math.pow(t - p[i], 2) / factorG1) +
+        factorG *
+          Math.exp(-Math.pow((t - p[i]) / p[i + nL * 2] / factor, 2) / 2) +
         factorL / (Math.pow(t - p[i], 2) + p2);
     }
     return result;
--- src/shapes/sumOfGaussians.js
+++ src/shapes/sumOfGaussians.js
@@ -7,12 +7,13 @@
  * @returns {*}
  */
 export function sumOfGaussians(p) {
+  const factor = 2 / Math.sqrt(2 * Math.log(2));
   return function (t) {
     let nL = p.length / 3;
     let result = 0;
     for (let i = 0; i < nL; i++) {
-      result +=
-        p[i + nL] * Math.exp(-Math.pow((t - p[i]) / p[i + nL * 2], 2) / 2);
+      let sd = p[i + nL * 2] / factor;
+      result += p[i + nL] * Math.exp(-Math.pow((t - p[i]) / sd, 2) / 2);
     }
     return result;
   };
