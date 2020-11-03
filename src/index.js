13aa294c6c50e43ee5c64a4d66fa95ebbc0383e4
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2020-11-03 11:38:27 -0500

    add peak-shape-generator to calculate the y value for each shape supported. close #23 close #5 (#25)
    * feat: add ml-peak-shape-generator
    
    * chore: split table in README
    
    * fix: close #22
    
    * feat !: just export one general method close #20 close #21
    Default behavior: all initial parameters should be passed as a list of {x, y, width} and width will be optimized as full width at half maximum (FWHM)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,107 @@
-export { optimize } from './optimize';
-export { optimizeSum } from './optimizeSum';
+import getMaxValue from 'ml-array-max';
+import LM from 'ml-levenberg-marquardt';
+
+import { sumOfGaussianLorentzians } from './shapes/sumOfGaussianLorentzians';
+import { sumOfGaussians } from './shapes/sumOfGaussians';
+import { sumOfLorentzians } from './shapes/sumOfLorentzians';
+
+const keys = ['x', 'y', 'width', 'mu'];
+/**
+ * Fits a set of points to the sum of a set of bell functions.
+ * @param {Object} input - An object containing the x and y data to be fitted.
+ * @param {Array} peakList - A list of initial parameters to be optimized. e.g. coming from a peak picking [{x, y, width}].
+ * @param {Object} [options = {}]
+ * @param {String} [options.kind = 'gaussian'] - kind of shape used to fitting, lorentzian, gaussian and pseudovoigt are supported.
+ * @param {Object} [options.lmOptions = {}] - options of ml-levenberg-marquardt optimization package.
+ * @returns {Object} - A object with fitting error and the list of optimized parameters { parameters: [ {x, y, width} ], error } if the kind of shape is pseudoVoigt mu parameter is optimized.
+ */
+export function optimize(data, peakList, options = {}) {
+  let { kind = 'gaussian', lmOptions = {} } = options;
+
+  let maxY = getMaxValue(data.y);
+  data.y.forEach((_, i, arr) => (arr[i] /= maxY));
+
+  let nbParams;
+  let paramsFunc;
+  switch (kind.toLowerCase()) {
+    case 'gaussian':
+      nbParams = 3;
+      paramsFunc = sumOfGaussians;
+      break;
+    case 'lorentzian':
+      nbParams = 3;
+      paramsFunc = sumOfLorentzians;
+      break;
+    case 'pseudovoigt':
+      nbParams = 4;
+      paramsFunc = sumOfGaussianLorentzians;
+      break;
+    default:
+      throw new Error('kind of shape is not supported');
+  }
+
+  let nL = peakList.length;
+  let pInit = new Float64Array(nL * nbParams);
+  let pMin = new Float64Array(nL * nbParams);
+  let pMax = new Float64Array(nL * nbParams);
+  let dt = Math.abs(data.x[0] - data.x[1]);
+
+  for (let i = 0; i < nL; i++) {
+    let peak = peakList[i];
+    for (let s = 0; s < nbParams; s++) {
+      pInit[i + s * nL] = getValue(s, peak, 'init', dt);
+      pMin[i + s * nL] = getValue(s, peak, 'min', dt);
+      pMax[i + s * nL] = getValue(s, peak, 'max', dt);
+    }
+  }
+
+  lmOptions = Object.assign(
+    {
+      damping: 1.5,
+      initialValues: pInit,
+      minValues: pMin,
+      maxValues: pMax,
+      gradientDifference: dt / 10000,
+      maxIterations: 100,
+      errorTolerance: 10e-5,
+    },
+    lmOptions,
+  );
+  let pFit = LM(data, paramsFunc, lmOptions);
+
+  let { parameterError: error, iterations } = pFit;
+  let result = { error, iterations, peaks: new Array(nL) };
+  for (let i = 0; i < nL; i++) {
+    let peak = {};
+    pFit.parameterValues[i + nL] *= maxY;
+    for (let s = 0; s < nbParams; s++) {
+      peak[keys[s]] = pFit.parameterValues[i + s * nL];
+    }
+    result.peaks[i] = peak;
+  }
+  return result;
+}
+
+function getValue(parameterIndex, peak, key, dt) {
+  let value;
+  switch (parameterIndex) {
+    case 0:
+      value =
+        key === 'init' ? peak.x : key === 'min' ? peak.x - dt : peak.x + dt;
+      break;
+    case 1:
+      value = key === 'init' ? 1 : key === 'min' ? 0 : 1.5;
+      break;
+    case 2:
+      value =
+        key === 'init'
+          ? peak.width
+          : key === 'min'
+          ? peak.width / 4
+          : peak.width * 4;
+      break;
+    default:
+      value = key === 'init' ? 0.5 : key === 'min' ? 0 : 1;
+  }
+  return value;
+}
