7b506a4db0fdc07bba26bccc33e0a6ba00336f40
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2021-01-19 22:06:59 -0500

    checkInput function (#49)
    * feat: add checkInput function
    
    * feat: first proposal to improve generalization
    
    * feat: improve documentation

---
+++ src/checkInput.js
@@ -0,0 +1,146 @@
+import merge from 'merge-deep';
+import getMaxValue from 'ml-array-max';
+
+import { sumOfGaussianLorentzians } from './shapes/sumOfGaussianLorentzians';
+import { sumOfGaussians } from './shapes/sumOfGaussians';
+import { sumOfLorentzians } from './shapes/sumOfLorentzians';
+
+export function checkInput(data, peaks, options) {
+  let {
+    shape = { kind: 'gaussian' },
+    optimization = {
+      kind: 'lm',
+    },
+  } = options;
+
+  if (typeof shape.kind !== 'string') {
+    throw new Error('kind should be a string');
+  }
+
+  let kind = shape.kind.toLowerCase().replace(/[^a-z]/g, '');
+
+  let paramsFunc;
+  let defaultParameters;
+  switch (kind) {
+    case 'gaussian':
+      paramsFunc = sumOfGaussians;
+      defaultParameters = {
+        x: {
+          init: (peak) => peak.x,
+          max: (peak) => peak.x + peak.width * 2,
+          min: (peak) => peak.x - peak.width * 2,
+          gradientDifference: (peak) => peak.width * 2e-3,
+        },
+        y: {
+          init: (peak) => peak.y,
+          max: () => 1.5,
+          min: () => 0,
+          gradientDifference: () => 1e-3,
+        },
+        width: {
+          init: (peak) => peak.width,
+          max: (peak) => peak.width * 2,
+          min: (peak) => peak.width * 0.5,
+          gradientDifference: (peak) => peak.width * 2e-3,
+        },
+      };
+      break;
+    case 'lorentzian':
+      paramsFunc = sumOfLorentzians;
+      defaultParameters = {
+        x: {
+          init: (peak) => peak.x,
+          max: (peak) => peak.x + peak.width * 2,
+          min: (peak) => peak.x - peak.width * 2,
+          gradientDifference: (peak) => peak.width * 2e-3,
+        },
+        y: {
+          init: (peak) => peak.y,
+          max: () => 1.5,
+          min: () => 0,
+          gradientDifference: () => 1e-3,
+        },
+        width: {
+          init: (peak) => peak.width,
+          max: (peak) => peak.width * 2,
+          min: (peak) => peak.width * 0.5,
+          gradientDifference: (peak) => peak.width * 2e-3,
+        },
+      };
+      break;
+    case 'pseudovoigt':
+      paramsFunc = sumOfGaussianLorentzians;
+      defaultParameters = {
+        x: {
+          init: (peak) => peak.x,
+          max: (peak) => peak.x + peak.width * 2,
+          min: (peak) => peak.x - peak.width * 2,
+          gradientDifference: (peak) => peak.width * 2e-3,
+        },
+        y: {
+          init: (peak) => peak.y,
+          max: () => 1.5,
+          min: () => 0,
+          gradientDifference: () => 1e-3,
+        },
+        width: {
+          init: (peak) => peak.width,
+          max: (peak) => peak.width * 2,
+          min: (peak) => peak.width * 0.5,
+          gradientDifference: (peak) => peak.width * 2e-3,
+        },
+        mu: {
+          init: (peak) => (peak.mu !== undefined ? peak.mu : 0.5),
+          min: () => 0,
+          max: () => 1,
+          gradientDifference: () => 0.01,
+        },
+      };
+      break;
+    default:
+      throw new Error('kind of shape is not supported');
+  }
+
+  let x = data.x;
+  let maxY = getMaxValue(data.y);
+  let y = new Array(x.length);
+  for (let i = 0; i < x.length; i++) {
+    y[i] = data.y[i] / maxY;
+  }
+
+  for (let i = 0; i < peaks.length; i++) {
+    peaks[i].y /= maxY;
+  }
+
+  let parameters = merge(optimization.parameters, defaultParameters);
+  for (let key in parameters) {
+    for (let par in parameters[key]) {
+      if (!Array.isArray(parameters[key][par])) {
+        parameters[key][par] = [parameters[key][par]];
+      }
+      if (
+        parameters[key][par].length !== 1 &&
+        parameters[key][par].length !== peaks.length
+      ) {
+        throw new Error(`The length of ${key}-${par} is not correct`);
+      }
+      for (let index = 0; index < parameters[key][par].length; index++) {
+        if (typeof parameters[key][par][index] === 'number') {
+          let value = parameters[key][par][index];
+          parameters[key][par][index] = () => value;
+        }
+      }
+    }
+  }
+
+  optimization.parameters = parameters;
+
+  return {
+    y,
+    x,
+    maxY,
+    peaks,
+    paramsFunc,
+    optimization,
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import getMaxValue from 'ml-array-max';
-
+import { checkInput } from './checkInput';
 import { selectMethod } from './selectMethod';
-import { sumOfGaussianLorentzians } from './shapes/sumOfGaussianLorentzians';
-import { sumOfGaussians } from './shapes/sumOfGaussians';
-import { sumOfLorentzians } from './shapes/sumOfLorentzians';
 
-const STATE_INIT = 0;
-const STATE_MIN = 1;
-const STATE_MAX = 2;
-const STATE_GRADIENT_DIFFERENCE = 3;
+// const STATE_INIT = 0;
+// const STATE_MIN = 1;
+// const STATE_MAX = 2;
+// const STATE_GRADIENT_DIFFERENCE = 3;
 
-const X = 0;
-const Y = 1;
-const WIDTH = 2;
-const MU = 3;
+// const X = 0;
+// const Y = 1;
+// const WIDTH = 2;
+// const MU = 3;
 
-const keys = ['x', 'y', 'width', 'mu'];
+// const keys = ['x', 'y', 'width', 'mu'];
 /**
  * Fits a set of points to the sum of a set of bell functions.
  * @param {object} data - An object containing the x and y data to be fitted.
@@ -25,18 +21,19 @@ const keys = ['x', 'y', 'width', 'mu'];
  * @param {string} [options.shape.kind = 'gaussian'] - kind of shape; lorentzian, gaussian and pseudovoigt are supported.
  * @param {object} [options.optimization = {}] - it's specify the kind and options of the algorithm use to optimize parameters.
  * @param {object} [options.optimization.kind = 'lm'] - kind of algorithm. By default it's levenberg-marquardt.
- * @param {number} [options.optimization.minFactorWidth = 0.25] - factor of width to define the lower limit of the width parameter.
- * @param {number} [options.optimization.maxFactorWidth = 4] - factor of width to define the upper limit of the width parameter.
- * @param {number} [options.optimization.minFactorX = 2] - factor of width to define the lower limit of the x parameter.
- * @param {number} [options.optimization.maxFactorX = 2] - factor of width to define the upper limit of the x parameter.
- * @param {number} [options.optimization.minFactorY = 0] - factor of width to define the upper limit of the y parameter.
- * @param {number} [options.optimization.maxFactorY = 1.5] - factor of width to define the lower limit of the y parameter.
- * @param {number} [options.optimization.minMuValue = 0] - minimum value of gaussian ratio.
- * @param {number} [options.optimization.maxMuValue = 1] - maximum value of gaussian ratio.
- * @param {number} [options.optimization.xGradientDifference] - value for gradient difference of x parameter.
- * @param {number} [options.optimization.yGradientDifference = 1e-3] - value for gradient difference of y parameter.
- * @param {number} [options.optimization.widthGradientDifference] - value for gradient difference of width parameter.
- * @param {number} [options.optimization.muGradientDifference = 0.01] - value for gradient difference of width parameter.
+ * @param {object} [options.optimization.parameters] - options of each parameter to be optimized e.g. For a gaussian shape
+ *  it could have x, y and with properties, each of which could contain init, min, max and gradientDifference, those options will define the guess,
+ *  the min and max value of the parameter (search space) and the step size to approximate the jacobian matrix respectively. Those options could be a number,
+ *  array of numbers, callback, or array of callbacks.
+ * @param {object} [options.optimization.parameters.x] - options for x parameter.
+ * @param {number|callback|array<number|callback>} [options.optimization.parameters.x.init] - definition of the starting point of the parameter (the guess),
+ *  if it is a callback the method pass the peak as the unique input, if it is an array the first element define the guess of the first peak and so on.
+ * @param {number|callback|array<number|callback>} [options.optimization.parameters.x.min] - definition of the lower limit of the parameter,
+ *  if it is a callback the method pass the peak as the unique input, if it is an array the first element define the min of the first peak and so on.
+ * @param {number|callback|array<number|callback>} [options.optimization.parameters.x.max] - definition of the upper limit of the parameter,
+ *  if it is a callback the method pass the peak as the unique input, if it is an array the first element define the max of the first peak and so on.
+ * @param {number|callback|array<number|callback>} [options.optimization.parameters.x.gradientDifference] - definition of  the step size to approximate the jacobian matrix of the parameter,
+ *  if it is a callback the method pass the peak as the unique input, if it is an array the first element define the gradientDifference of the first peak and so on.
  * @param {object} [options.optimization.options = {}] - options for the specific kind of algorithm.
  * @param {number} [options.optimization.options.timeout] - maximum time running before break in seconds.
  * @param {number} [options.optimization.options.damping=1.5]
@@ -44,95 +41,37 @@ const keys = ['x', 'y', 'width', 'mu'];
  * @param {number} [options.optimization.options.errorTolerance=1e-8]
  * @returns {object} - A object with fitting error and the list of optimized parameters { parameters: [ {x, y, width} ], error } if the kind of shape is pseudoVoigt mu parameter is optimized.
  */
-export function optimize(data, peaks, options = {}) {
-  let {
-    shape = { kind: 'gaussian' },
-    optimization = {
-      kind: 'lm',
-    },
-  } = options;
+export function optimize(data, peakList, options = {}) {
+  const { y, x, maxY, peaks, paramsFunc, optimization } = checkInput(
+    data,
+    peakList,
+    options,
+  );
 
-  let {
-    minFactorWidth = 0.25,
-    maxFactorWidth = 4,
-    minFactorX = 2,
-    maxFactorX = 2,
-    minFactorY = 0,
-    maxFactorY = 1.5,
-    minMuValue = 0,
-    maxMuValue = 1,
-    xGradientDifference,
-    yGradientDifference = 1e-3,
-    widthGradientDifference,
-    muGradientDifference = 0.01,
-  } = optimization;
+  let parameters = optimization.parameters;
 
-  peaks = JSON.parse(JSON.stringify(peaks));
-
-  if (typeof shape.kind !== 'string') {
-    throw new Error('kind should be a string');
-  }
-
-  let kind = shape.kind.toLowerCase().replace(/[^a-z]/g, '');
-
-  let x = data.x;
-  let maxY = getMaxValue(data.y);
-  let y = new Array(x.length);
-  for (let i = 0; i < x.length; i++) {
-    y[i] = data.y[i] / maxY;
-  }
-
-  let nbParams;
-  let paramsFunc;
-  switch (kind) {
-    case 'gaussian':
-      nbParams = 3;
-      paramsFunc = sumOfGaussians;
-      break;
-    case 'lorentzian':
-      nbParams = 3;
-      paramsFunc = sumOfLorentzians;
-      break;
-    case 'pseudovoigt':
-      nbParams = 4;
-      paramsFunc = sumOfGaussianLorentzians;
-      break;
-    default:
-      throw new Error('kind of shape is not supported');
-  }
-
-  const getValueOptions = {
-    maxY,
-    minFactorX,
-    maxFactorX,
-    minFactorY,
-    maxFactorY,
-    minMuValue,
-    maxMuValue,
-    minFactorWidth,
-    maxFactorWidth,
-    xGradientDifference,
-    yGradientDifference,
-    widthGradientDifference,
-    muGradientDifference,
-  };
   let nbShapes = peaks.length;
-  let pMin = new Float64Array(nbShapes * nbParams);
-  let pMax = new Float64Array(nbShapes * nbParams);
-  let pInit = new Float64Array(nbShapes * nbParams);
-  let gradientDifference = new Float64Array(nbShapes * nbParams);
+  let parameterKey = Object.keys(parameters);
+  let nbParams = nbShapes * parameterKey.length;
+  let pMin = new Float64Array(nbParams);
+  let pMax = new Float64Array(nbParams);
+  let pInit = new Float64Array(nbParams);
+  let gradientDifference = new Float64Array(nbParams);
+
   for (let i = 0; i < nbShapes; i++) {
     let peak = peaks[i];
-    for (let s = 0; s < nbParams; s++) {
-      pInit[i + s * nbShapes] = getValue(s, peak, STATE_INIT, getValueOptions);
-      pMin[i + s * nbShapes] = getValue(s, peak, STATE_MIN, getValueOptions);
-      pMax[i + s * nbShapes] = getValue(s, peak, STATE_MAX, getValueOptions);
-      gradientDifference[i + s * nbShapes] = getValue(
-        s,
-        peak,
-        STATE_GRADIENT_DIFFERENCE,
-        getValueOptions,
-      );
+    for (let k = 0; k < parameterKey.length; k++) {
+      let key = parameterKey[k];
+      let init = parameters[key].init;
+      let min = parameters[key].min;
+      let max = parameters[key].max;
+      let gradientDifferenceValue = parameters[key].gradientDifference;
+      pInit[i + k * nbShapes] = init[i % init.length](peak);
+      pMin[i + k * nbShapes] = min[i % min.length](peak);
+      pMax[i + k * nbShapes] = max[i % max.length](peak);
+      gradientDifference[i + k * nbShapes] = gradientDifferenceValue[
+        i % gradientDifferenceValue.length
+      ](peak);
     }
   }
 
@@ -147,85 +86,13 @@ export function optimize(data, peaks, options = {}) {
 
   let { parameterError: error, iterations } = pFit;
   let result = { error, iterations, peaks };
-  for (let i = 0; i < peaks.length; i++) {
-    pFit.parameterValues[i + peaks.length] *= maxY;
-    for (let s = 0; s < nbParams; s++) {
+  for (let i = 0; i < nbShapes; i++) {
+    pFit.parameterValues[i + nbShapes] *= maxY;
+    for (let k = 0; k < parameterKey.length; k++) {
       // we modify the optimized parameters
-      peaks[i][keys[s]] = pFit.parameterValues[i + s * peaks.length];
+      peaks[i][parameterKey[k]] = pFit.parameterValues[i + k * nbShapes];
     }
   }
 
   return result;
-}
-
-function getValue(parameterIndex, peak, state, options) {
-  let maxY = options.maxY;
-  switch (state) {
-    case STATE_INIT:
-      switch (parameterIndex) {
-        case X:
-          return peak.x;
-        case Y:
-          return peak.y / maxY;
-        case WIDTH:
-          return peak.width;
-        case MU:
-          return peak.mu || 0.5;
-        default:
-          throw new Error('The parameter is not supported');
-      }
-    case STATE_GRADIENT_DIFFERENCE:
-      switch (parameterIndex) {
-        case X:
-          return peak.xGradientDifference !== undefined
-            ? peak.xGradientDifference
-            : options.xGradientDifference !== undefined
-            ? options.xGradientDifference
-            : peak.width / 2e3;
-        case Y:
-          return peak.yGradientDifference !== undefined
-            ? peak.yGradientDifference
-            : options.yGradientDifference;
-        case WIDTH:
-          return peak.widthGradientDifference !== undefined
-            ? peak.widthGradientDifference
-            : options.widthGradientDifference !== undefined
-            ? options.widthGradientDifference
-            : peak.width / 2e3;
-        case MU:
-          return peak.muGradientDifference !== undefined
-            ? peak.muGradientDifference
-            : options.muGradientDifference;
-        default:
-          throw new Error('The parameter is not supported');
-      }
-    case STATE_MIN:
-      switch (parameterIndex) {
-        case X:
-          return peak.x - peak.width * options.minFactorX;
-        case Y:
-          return (peak.y / maxY) * options.minFactorY;
-        case WIDTH:
-          return peak.width * options.minFactorWidth;
-        case MU:
-          return options.minMuValue;
-        default:
-          throw new Error('The parameter is not supported');
-      }
-    case STATE_MAX:
-      switch (parameterIndex) {
-        case X:
-          return peak.x + peak.width * options.maxFactorX;
-        case Y:
-          return (peak.y / maxY) * options.maxFactorY;
-        case WIDTH:
-          return peak.width * options.maxFactorWidth;
-        case MU:
-          return options.maxMuValue;
-        default:
-          throw new Error('The parameter is not supported');
-      }
-    default:
-      throw Error('the state is not supported');
-  }
 }
