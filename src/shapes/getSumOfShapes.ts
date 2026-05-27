8b67e00b61cc5e495726c0c1a201017086ce6cfd
jobo322 <jose.bolanos@correounivalle.edu.co>
2026-05-27 09:42:40 -0500

    feat: improve speed do not update peak.shapeFct[...] every time the returned function is evaluated at an x

--- src/shapes/getSumOfShapes.ts
+++ src/shapes/getSumOfShapes.ts
@@ -8,19 +8,21 @@ import type { InternalPeak } from '../util/internalPeaks/getInternalPeaks.ts';
 
 export function getSumOfShapes(internalPeaks: InternalPeak[]) {
   return function sumOfShapes(parameters: number[]) {
+    for (const peak of internalPeaks) {
+      for (let i = 2; i < parameters.length; i++) {
+        type Parameter = (typeof peak.parameters)[number];
+        const shapeFctKey = peak.parameters[i] as Extract<
+          Parameter,
+          keyof typeof peak.shapeFct
+        >;
+        peak.shapeFct[shapeFctKey] = parameters[peak.fromIndex + i];
+      }
+    }
     return (x: number) => {
       let totalY = 0;
       for (const peak of internalPeaks) {
         const peakX = parameters[peak.fromIndex];
         const y = parameters[peak.fromIndex + 1];
-        for (let i = 2; i < parameters.length; i++) {
-          type Parameter = (typeof peak.parameters)[number];
-          const shapeFctKey = peak.parameters[i] as Extract<
-            Parameter,
-            keyof typeof peak.shapeFct
-          >;
-          peak.shapeFct[shapeFctKey] = parameters[peak.fromIndex + i];
-        }
         totalY += y * peak.shapeFct.fct(x - peakX);
       }
       return totalY;
